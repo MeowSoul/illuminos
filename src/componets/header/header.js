bac5b481557162c18c9777ffa08fec079f30e55e
AgentHumer <agent_hammer@mail.ru>
2023-11-13 23:14:40 +0500

    Рефакторинг и доведение до ума с помщью UI и props

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import styles from './header.module.css'
+import MenuItem from "../UI/menuItem/menuItem";
 
 const Header = () => {
     return (
-        <div>
-            <header className={styles.header}>
-                <div>
-                    <img src="" alt="logo"/>
-                    <div>Главная</div>
-                    <div>Фильмы</div>
-                    <div>Сериалы</div>
-                </div>
-                <div>
-                    <div>Купить подписку</div>
-                    <img src="" alt="logo__user"/>
-                </div>
-            </header>
+        <div className={styles.main}>
+            <div className={styles.nav}>
+                <img src="" alt="Logo"/>
+                <MenuItem menuTextItem = "Главная" menuLink = "/"/>
+                <MenuItem menuTextItem = "Фильмы" menuLink = " "/>
+                <MenuItem menuTextItem = "Сериалы" menuLink = " "/>
+            </div>
+            <div className={styles.buttons}>
+                <button>Купить подписку</button>
+                <img src="" alt="Logo-user"/>
+            </div>
         </div>
     );
 };
