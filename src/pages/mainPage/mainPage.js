2e113e13f4b3a002f88424d7408a5f600d385025
AgentHumer <agent_hammer@mail.ru>
2023-11-13 23:17:12 +0500

    Рефакторинг и добавление элементов для первого блока

--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import Header from '../../componets/header/header'
 import styles from  './mainPage.module.css'
+import Header from "../../componets/header/header";
+import SubscriptionText from "../../componets/UI/mainText/subscriptionText";
 
 const MainPage = () => {
     return (
-        <div className={styles.wrapper}>
+        <div>
             <Header/>
+            <main className={styles.main}>
+                <div className={styles.circle}></div>
+                <div>
+                    <div>
+                        <SubscriptionText text = "Подписка для лучших"></SubscriptionText>
+                        <SubscriptionText text = "Смотрите кино, сериалы и спорт"></SubscriptionText>
+                        <SubscriptionText text = "Месяц за - 1 ₽ дальше - 100 ₽/месяц"></SubscriptionText>
+                    </div>
+                    <div>
+                        <button>Купить подписку</button>
+                        <button>Подробнее</button>
+                    </div>
+                </div>
+                <div></div>
+            </main>
         </div>
     );
 };
