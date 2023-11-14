c601bd33a92f64772507caeef3b149a4b2f1b00d
meowsoul <agent_hammer@mail.ru>
2023-11-14 13:00:29 +0500

    Рефакторинг

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './header.module.css'
-import MenuItem from "../UI/menuItem/menuItem";
+import MenuItem from "./UI/menuItem/menuItem";
 
 const Header = () => {
     return (
         <div className={styles.main}>
             <div className={styles.nav}>
-                <img src="" alt="Logo"/>
+                <img src={"/image/logo.png"} alt="Logo"/>
                 <MenuItem menuTextItem = "Главная" menuLink = "/"/>
-                <MenuItem menuTextItem = "Фильмы" menuLink = " "/>
+                <MenuItem menuTextItem = "Фильмы" menuLink = "/MoviePage"/>
                 <MenuItem menuTextItem = "Сериалы" menuLink = " "/>
             </div>
             <div className={styles.buttons}>
-                <button>Купить подписку</button>
-                <img src="" alt="Logo-user"/>
+                <button className={styles.subscribe}>Купить подписку</button>
+                <img src={"/image/user-logo.png"} alt="Logo-user"/>
             </div>
         </div>
     );
