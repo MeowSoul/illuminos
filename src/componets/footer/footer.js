ff351ed2a61214e162c7ffcab9d2b8a8ec8681a5
meowsoul <agent_hammer@mail.ru>
2023-11-15 17:22:47 +0500

    Создание и стилизация footer

---
+++ src/componets/footer/footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './footer.module.css'
+const Footer = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.icons}>
+                <div className={styles.icons__social}>
+
+                </div>
+                <div className={styles.icons__payment}>
+
+                </div>
+                <div className={styles.icons__illuminos}>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Footer;
