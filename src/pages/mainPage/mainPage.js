e8c696708a64ab29d8911fe195e091c257653c0c
AgentHumer <agent_hammer@mail.ru>
2023-11-15 12:17:31 +0500

    Рефактор

--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -3,7 +3,7 @@ import styles from  './mainPage.module.css'
 import Header from "../../componets/header/header";
 import SubscriptionText from "./UI/mainText/subscriptionText";
 import MainCards from "./UI/mainCards/mainCards";
-import MainGenres from "./UI/mainGenres/mainGenres";
+import MainFilter from "./UI/mainFilter/mainFilter";
 import MainSorting from "./UI/mainSorting/mainSorting";
 
 const MainPage = () => {
@@ -13,9 +13,9 @@ const MainPage = () => {
             <div className={styles.main}>
                 <div className={styles.circle}></div>
                 <div className={styles.main__text}>
-                    <SubscriptionText text = "Подписка для лучших"/>
-                    <SubscriptionText text = "Смотрите кино, сериалы и спорт"/>
-                    <SubscriptionText text = "Месяц за - 1 ₽ дальше - 100 ₽/месяц"/>
+                    <SubscriptionText subText = "Подписка для лучших"/>
+                    <SubscriptionText subText = "Смотрите кино, сериалы и спорт"/>
+                    <SubscriptionText subText = "Месяц за - 1 ₽ дальше - 100 ₽/месяц"/>
                     <div className={styles.buttons}>
                         <button className={styles.button_yellow}>Купить подписку</button>
                         <button className={styles.button_black}>Подробнее</button>
@@ -23,7 +23,7 @@ const MainPage = () => {
                 </div>
                 <MainCards/>
             </div>
-            <MainGenres/>
+            <MainFilter/>
             <MainSorting/>
         </div>
     );
