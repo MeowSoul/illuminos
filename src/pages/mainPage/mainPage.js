0004b7357641fced541b1194369ed8dc709eae90
meowsoul <agent_hammer@mail.ru>
2023-11-17 11:11:43 +0500

    создание JSON объекта filmData

--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,34 +1,48 @@
-import React from 'react';
-import styles from  './mainPage.module.css'
-import Header from "../../componets/header/header";
-import SubscriptionText from "./UI/mainText/subscriptionText";
-import MainCards from "./UI/mainCards/mainCards";
-import MainFilter from "./UI/mainFilter/mainFilter";
-import MainSorting from "./UI/mainSorting/mainSorting";
-import Footer from "../../componets/footer/footer";
+import React from "react"
+import styles from "./mainPage.module.css"
+import Header from "../../componets/header/header"
+import SubscriptionText from "./UI/mainText/subscriptionText"
+import MainCards from "./UI/mainCards/mainCards"
+import MainFilter from "./UI/mainFilter/mainFilter"
+import MainSorting from "./UI/mainSorting/mainSorting"
+import Footer from "../../componets/footer/footer"
+
+export const filmData = [
+    {
+        id: 1,
+        name: "Джокер",
+        rating: 9.8,
+        year: 2007,
+        genre: "Триллер",
+        time: "1 ч 36 мин",
+        voice: ["Rus", "Eng"],
+        ageLimit: 18,
+        imgName: "joker.jpg",
+    },
+]
 
 const MainPage = () => {
     return (
         <div>
-            <Header/>
+            <Header />
             <div className={styles.main}>
                 <div className={styles.circle}></div>
                 <div className={styles.main__text}>
-                    <SubscriptionText subText = "Подписка для лучших"/>
-                    <SubscriptionText subText = "Смотрите кино, сериалы и спорт"/>
-                    <SubscriptionText subText = "Месяц за - 1 ₽ дальше - 100 ₽/месяц"/>
+                    <SubscriptionText subText="Подписка для лучших" />
+                    <SubscriptionText subText="Смотрите кино, сериалы и спорт" />
+                    <SubscriptionText subText="Месяц за - 1 ₽ дальше - 100 ₽/месяц" />
                     <div className={styles.buttons}>
                         <button className={styles.button_yellow}>Купить подписку</button>
                         <button className={styles.button_black}>Подробнее</button>
                     </div>
                 </div>
-                <MainCards/>
+                <MainCards films={filmData} />
             </div>
-            <MainFilter/>
-            <MainSorting/>
-            <Footer/>
+            <MainFilter />
+            <MainSorting />
+            <Footer />
         </div>
-    );
-};
+    )
+}
 
-export default MainPage;
+export default MainPage
