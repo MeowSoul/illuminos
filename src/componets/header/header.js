3fbb65f8fbd67cca8ef127d08810032756a019de
meowsoul <agent_hammer@mail.ru>
2023-11-17 11:10:44 +0500

    Рефакторинг

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import styles from './header.module.css'
-import MenuItem from "./UI/menuItem/menuItem";
+import React from "react"
+import styles from "./header.module.css"
+import MenuItem from "./UI/menuItem/menuItem"
 
 const Header = () => {
     return (
         <div className={styles.main}>
             <div className={styles.nav}>
-                <img src={"/image/logo.png"} alt="Logo"/>
-                <MenuItem menuTextItem = "Главная" menuLink = "/"/>
-                <MenuItem menuTextItem = "Фильмы" menuLink = "/MoviePage"/>
-                <MenuItem menuTextItem = "Сериалы" menuLink = " "/>
+                <img src={"/image/logo.png"} alt="Logo" />
+                <MenuItem menuTextItem="Главная" menuLink="/" />
+                <MenuItem menuTextItem="Фильмы" menuLink="/movie/:id" />
+                <MenuItem menuTextItem="Сериалы" menuLink=" " />
             </div>
             <div className={styles.buttons}>
                 <button className={styles.subscribe}>Купить подписку</button>
-                <img src={"/image/user-logo.png"} alt="Logo-user"/>
+                <img src={"/image/user-logo.png"} alt="Logo-user" />
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Header;
+export default Header
