9e6b4f26b100ba4eb1539a157c264aa27d1ecd24
meowsoul <agent_hammer@mail.ru>
2023-11-17 11:13:54 +0500

    Добавление на стрницу MoviePage header ,main ,footer

--- src/pages/moviePage/moviePage.js
+++ src/pages/moviePage/moviePage.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React from "react"
+import Header from "../../componets/header/header"
+import Footer from "../../componets/footer/footer"
+import Banner from "../../componets/banner/banner"
+import { filmData } from "../mainPage/mainPage"
 const MoviePage = () => {
     return (
         <div>
-            
+            <Header />
+            <main>
+                <Banner bannerName={"banner.jpg"} ratingMovie={filmData[0].rating} yearMovie={filmData[0].year}/>
+            </main>
+            <Footer />
         </div>
-    );
-};
+    )
+}
 
-export default MoviePage;
+export default MoviePage
