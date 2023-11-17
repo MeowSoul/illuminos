a9dbfdac08196218b7e52c662d1ab57f312f920f
meowsoul <agent_hammer@mail.ru>
2023-11-17 11:06:55 +0500

    Изменен роутинг

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import MainPage from './pages/mainPage/mainPage';
-import MoviePage from './pages/moviePage/moviePage'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import MainPage from "./pages/mainPage/mainPage"
+import MoviePage from "./pages/moviePage/moviePage"
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/MoviePage" element={<MoviePage />} />
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/movie/:id" element={<MoviePage />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
