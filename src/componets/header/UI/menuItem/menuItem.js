f53ff893634c1266cb4ee56b02ad5f2ae646d67f
meowsoul <agent_hammer@mail.ru>
2023-11-17 11:10:06 +0500

    Рефакторинг

--- src/componets/header/UI/menuItem/menuItem.js
+++ src/componets/header/UI/menuItem/menuItem.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import styles from './menuItem.module.css'
+import React from "react"
+import { Link } from "react-router-dom"
+import styles from "./menuItem.module.css"
 
-const MenuItem = ({menuLink, menuTextItem}) => {
+const MenuItem = ({ menuLink, menuTextItem }) => {
     return (
-        <Link to = {menuLink} className={styles.link}>{menuTextItem}</Link>
-    );
-};
+        <Link to={menuLink} className={styles.link}>
+            {menuTextItem}
+        </Link>
+    )
+}
 
-export default MenuItem;
+export default MenuItem
