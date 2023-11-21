7fc28da794fdb2917951f89c366849513daadb8c
meowsoul <agent_hammer@mail.ru>
2023-11-21 18:52:12 +0500

    Рефакторинг

--- src/componets/banner/banner.js
+++ src/componets/banner/banner.js
@@ -12,23 +12,21 @@ const Banner = ({
     ageMovie,
 }) => {
     return (
-        <div
-            className={styles.main}
-            style={{
-                backgroundImage: `url("/image/${bannerName}")`,
-            }}
-        >
+        <div className={styles.main} style={{ backgroundImage: `url("/image/${bannerName}")` }}>
             <div className={styles.gradient}>
                 <div className={styles.information}>
                     <img src={`/image/${logoMovieName}`} alt="" className={styles.logo} />
-                    <div>
+                    <div className={styles.general__information}>
                         <div>{ratingMovie}</div>
                         <div>{yearMovie}</div>
                         <div>{genreMovie}</div>
                         <div>{timeMovie}</div>
                         <div>{languageMovie}</div>
-                        <div>{ageMovie}</div>
+                        <div>{ageMovie}+</div>
                     </div>
+                    <div className={styles.funFact}>За этот фильм Квентин Тарантино получил свой второй «Оскар»</div>
+                    <div className={styles.director}>Режиссёр:Квентин Тарантино</div>
+                    <div className={styles.actors}>Актёры:Джейми Фокс, Кристоф Вальц, Леонардо ДиКаприо</div>
                 </div>
             </div>
         </div>
