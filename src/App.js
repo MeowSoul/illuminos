4695313ef4cba0f5efcaee0429370fdfd96c2434
AgentHumer <agent_hammer@mail.ru>
2023-11-23 22:56:08 +0500

    Маршрутизация на странциу регистрации и и подписок

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MainPage from "./pages/mainPage/mainPage"
 import MoviePage from "./pages/moviePage/moviePage"
 import SeriesPage from "./pages/serialPage/seriesPage"
+import RegistrationPage from "./pages/registrationPage/registrationPage"
+import SubscriptionPage from "./pages/subscriptionPage/subscriptionPage"
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
                 <Route path="/" element={<MainPage />} />
                 <Route path="/movie/:id" element={<MoviePage />} />
                 <Route path="/series/:id" element={<SeriesPage />} />
+                <Route path="/registration" element={<RegistrationPage />} />
+                <Route path="/subscription" element={<SubscriptionPage />} />
             </Routes>
         </BrowserRouter>
     )
