919e2386799fa06ffcbaf34bdf4955e8950a902c
AgentHumer <agent_hammer@mail.ru>
2023-11-25 00:16:31 +0500

    Создание сервиса для аlert

---
+++ src/services/alertServices.js
@@ -0,0 +1,29 @@
+import { toast } from "react-toastify"
+
+export class AlertService {
+    static success(message) {
+        toast.success(message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        })
+    }
+
+    static error(message) {
+        toast.error(message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        })
+    }
+}
