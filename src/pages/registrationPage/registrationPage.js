8fe63f65cabd52a0fb3201d5545d71a88e3d1811
AgentHumer <agent_hammer@mail.ru>
2023-11-25 00:17:46 +0500

    Добавлена логика авторизации

--- src/pages/registrationPage/registrationPage.js
+++ src/pages/registrationPage/registrationPage.js
@@ -1,27 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
 import Footer from "../../componets/footer/footer"
 import styles from "./registrationPage.module.css"
 import MinHeader from "../../componets/min-header/min-header"
-import Userfront, { SignupForm } from "@userfront/toolkit/react"
-
-Userfront.init("demo1234")
+import {UserServices} from "../../services/userServices";
+import {AlertService} from "../../services/alertServices";
 
 const RegistrationPage = () => {
+
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function submitHandler () {
+        const result = await UserServices.registrationAsync(login, password)
+        if (result == null) {
+            return AlertService.success("УСПЕШНО!!!!!!!!!!!!!!!!!!")
+        }
+
+        return AlertService.error(result.errorMessage)
+    }
     return (
         <div>
             <main className={styles.main}>
                 <MinHeader />
                 <div className={styles.main_form}>
-                    <form action="" className={styles.form}>
+                    <div className={styles.form} >
                         <label className={styles.title}>Регистрация</label>
-                        <input className={styles.field} type="text" placeholder={"Имя"} />
-                        <input className={styles.field} type="text" placeholder={"Фамилия"} />
-                        <input className={styles.field} type="text" placeholder={"Логин"} />
-                        <input className={styles.field} type="text" placeholder={"Пароль"} />
-                        <button className={styles.button} type={"submit"}>
+                        <input className={styles.field} type="text" placeholder={"Логин"} onChange={(e) => setLogin(e.target.value)}/>
+                        <input className={styles.field} type="text" placeholder={"Пароль"} onChange={(e) => setPassword(e.target.value)} />
+                        <button className={styles.button} type={"submit"} onClick={ async () => submitHandler()}>
                             Отправить
                         </button>
-                    </form>
+                    </div>
                 </div>
                 <Footer />
             </main>
